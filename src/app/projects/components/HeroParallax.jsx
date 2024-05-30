e62fd283b28ece407b323f420859cb8b7dee21a5
lomi9 <119067005+lomi9@users.noreply.github.com>
2024-05-30 15:51:39 +0200

    chore : escape ' with &#39;

--- src/app/projects/components/HeroParallax.jsx
+++ src/app/projects/components/HeroParallax.jsx
@@ -92,10 +92,10 @@ const Header = () => {
   return (
     <div className=" max-w-screen-sm sm:max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full left-0 top-0">
       <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
-        Coup d'oeil <br /> Sur mes projets
+        Coup d&#39;oeil <br /> Sur mes projets
       </h1>
       <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
-        Je construis chacun de mes projets en utilisant les dernières technologies. Étant passionnée, je m'efforce de vous livrer un site web moderne, en portant une grande attention aux détails.
+        Je construis chacun de mes projets en utilisant les dernières technologies. Étant passionnée, je m&#39;efforce de vous livrer un site web moderne, en portant une grande attention aux détails.
       </p>
     </div>
   );
