25ff2e65d58e32e148d714e6c96d741e0090ad02
lomi9 <119067005+lomi9@users.noreply.github.com>
2024-05-31 15:06:38 +0200

    feat : add animate title in prices page

---
+++ src/app/prices/components/PricesTitle.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { FlipWords } from "./FlipWords";
+import { Info } from "lucide-react";
+
+export function PricesTitle() {
+  const words = ["raisonnables", "équilibrés", "transparents"];
+  const [isTooltipVisible, setIsTooltipVisible] = useState(false);
+
+  const showTooltip = () => setIsTooltipVisible(true);
+  const hideTooltip = () => setIsTooltipVisible(false);
+
+  return (
+    <div className="h-auto w-full flex justify-start items-center px-4 py-[5vw]">
+      <div className="text-2xl md:text-3xl lg:text-4xl w-full font-normal text-neutral-600 dark:text-neutral-400 relative">
+        Des tarifs
+        <FlipWords words={words} /> <br />
+        pour un projet correspondant à vos attentes. 
+        <span
+          className="relative inline-block ml-2"
+          onMouseEnter={showTooltip}
+          onMouseLeave={hideTooltip}
+          onClick={showTooltip}
+        >
+          <Info className="inline-block w-6 h-6 text-neutral-600 dark:text-neutral-400 cursor-pointer" />
+          {isTooltipVisible && (
+            <span className="absolute z-20  w-[400px] right-0 top-full mb-2 text-sm text-foreground bg-background border border-solid border-foreground/40 p-2 rounded">
+           Je m'engage à vous fournir un site web performant et à la hauteur de vos attentes. <br/> La création d'un site web, quelle que soit la technologie utilisée, nécessite du temps, de l'expertise, et de l'investissement.  <br/> Par conséquent, les tarifs sont établis en fonction du temps estimé pour réaliser votre projet, et des coûts associés à sa mise en œuvre.
+            </span>
+          )}
+        </span>
+      </div>
+    </div>
+  );
+}
--- src/app/prices/page.jsx
+++ src/app/prices/page.jsx
@@ -2,6 +2,7 @@
 
 import { useTheme } from "@/contexts/ThemeContext";
 import PricingTabs from "./components/PricingTabs";
+import { PricesTitle } from "./components/PricesTitle";
 
 
 const PricesPage = () => {
@@ -10,7 +11,8 @@ const PricesPage = () => {
 
     
     return (
-        <div className="w-screen flex justify-center items-center px-4 md:px-12 lg:px-18 pt-[6rem]">
+        <div className="max-w-[1200px] w-screen flex flex-wrap justify-center items-center px-4 md:px-12 lg:px-18 pt-[6rem]">
+            <PricesTitle/>
             <div className={`flex w-full justify-center items-center bg-opacity-0 border border-transparent border-opacity-00 rounded-xl backdrop-blur-[25px] backdrop-saturate-[1.06] ${theme === 'dark' ? 'bg-transparent' : 'bg-transparent'}`}>
                 <PricingTabs/>
             </div>
