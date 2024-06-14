c8b1683c05b7a21036b056847fa0330c0ac390c7
lomi9 <119067005+lomi9@users.noreply.github.com>
2024-06-14 19:20:38 +0200

    style : enhance modal on mobile

--- src/app/projects/components/Modal.jsx
+++ src/app/projects/components/Modal.jsx
@@ -17,12 +17,12 @@ const Modal = ({ product, onClose }) => {
   return (
     <section className="modal-screen-container flex flex-wrap fixed w-screen h-screen top-0 left-0 items-center justify-center z-50">
       <div className="modal-screen-background relative w-full h-full flex flex-wrap items-center bg-foreground opacity-60" onClick={onClose}></div>
-      <div className="modal-screen-position mt-4 h-[85vh] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 px-8 pb-8 rounded z-10 w-full">
+      <div className="modal-screen-position mt-4 h-[85vh] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 px-2 sm:px-8 pb-8 rounded z-10 w-full">
         <div className="relative modal-background mt-4 w-full h-[85vh] flex flex-wrap rounded px-8 pb-8 pt-2 border border-solid border-cardbordercolor/20">
           <div className="absolute w-full h-full top-0 left-0 rounded bg-background/60 backdrop-blur-[25px] backdrop-saturate-[1.06]"></div>
-          <div className="modal-title-container pt-6 relative w-full flex pb-8 flex-wrap justify-between lg:justify-center items-center"> 
+          <div className="modal-title-container pt-12 sm:pt-6 relative w-full flex pb-8 flex-wrap justify-between lg:justify-center items-center"> 
             <div className="w-full flex flex-wrap justify-center items-center">
-              <h2 className="modal-title text-2xl font-bold pr-4 z-20">{product.title}</h2>
+              <h2 className="modal-title text-2xl font-bold pr-2 sm:pr-4 z-20">{product.title}</h2>
               <a href={product.link}><ArrowUpRight/></a>
             </div>
             <div className="w-full flex flex-wrap justify-center items-center py-2">
