dc1ad45ced952da32bf64bf074cc72c3cb5ce147
lomi9 <119067005+lomi9@users.noreply.github.com>
2024-06-16 12:47:17 +0200

    fix : update prices tab

--- src/app/prices/components/PricingTabs.jsx
+++ src/app/prices/components/PricingTabs.jsx
@@ -42,6 +42,10 @@ export default function PricingTabs() {
 
   return (
     <div className='bg-transparent w-full max-w-[1200px] pb-10'>
+      
+      {/* Accordéon mobile */}
+
+
       <nav className="sm:hidden bg-transparent w-full sticky top-0 z-10">
         {tabs.map((tab) => (
           <div key={tab.name} className='bg-transparent w-full my-6'>
@@ -49,6 +53,8 @@ export default function PricingTabs() {
               ref={el => tabRefs.current[tab.name] = el}
               onClick={() => toggleAccordion(tab.name)}
               className={`p-4 rounded-full w-full flex justify-between relative text-foreground text-sm hover:shadow-2xl hover:shadow-white/[0.1] transition duration-200 border border-cardbordercolor ${theme === 'dark' ? 'bg-black/30' : 'bg-white/30'}`}>
+              <div className="absolute inset-x-0 h-px w-1/2 mx-auto -top-px shadow-2xl  bg-gradient-to-r from-transparent via-teal-300 to-transparent" />
+              <div className="absolute inset-x-0 h-px w-1/2 mx-auto -bottom-px shadow-2xl  bg-gradient-to-r from-transparent via-cyan-200 to-transparent" />
               <h2 className="relative z-20 flex w-full justify-between">
                 {tab.name}
                 <CircleArrowRight className={`text-teal-400 transition-transform duration-300 ${openTab === tab.name ? 'transform rotate-90' : ''}`} />
@@ -62,6 +68,37 @@ export default function PricingTabs() {
           </div>
         ))}
       </nav>
+
+          {/* Onglets pour les autres tailles d'écran */}
+  <div className="hidden sm:flex flex-wrap justify-center items-center">
+    <nav className="border-b border-tagsborder">
+      <ul className="-mb-px flex gap-6" aria-label="Tabs">
+        {tabs.map((tab) => (
+          <li key={tab.name} className="mr-2 relative">
+            <h2
+              className={`inline-block cursor-pointer p-4 rounded-t-lg ${
+                activeTab === tab.name
+                  ? "shrink-0 border-b-2 border-b-transparent px-1 pb-4 text-sm font-medium text-tags"
+                  : "text-gray-500 shrink-0 border-transparent px-1 pb-4 text-sm font-medium hover:text-tagshover "
+              }`}
+              onClick={() => setActiveTab(tab.name)}
+            >
+              {tab.name}
+            </h2>
+            {activeTab === tab.name && (
+                <div className={`active-teal-line absolute inset-x-0 h-px w-3/4 mx-auto -bottom-px shadow-2xl  bg-gradient-to-r from-transparent to-transparent ${theme === 'dark' ? 'via-teal-400' : 'via-teal-700'}`}>
+                
+                </div>
+            )}
+          </li>
+        ))}
+      </ul>
+    </nav>
+    <article className="p-4">
+      {tabs.find(tab => tab.name === activeTab).component}
+    </article>
+  </div>
+
     </div>
   );
 }
